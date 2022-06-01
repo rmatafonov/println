e0b7f9a486919c7af51d37e45a061836bbf0d1d1
Alexander Senin <isavlex@yandex.ru>
2022-06-01 11:15:12 +0300

    Добавил компонент обработки ошибок для дочерних компонентов - ErrorBoundary, добавил config для pre-commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,13 @@
 import React, { Component } from 'react'
-import Button from '../Button'
+import ErrorBoundary from '../ErrorBoundary'
+import Bug from '../Bug'
 
 class App extends Component {
-  state = {
-    name: 'Пупа',
-    dummy: false,
-  }
-
-  toggleMessage = () => {
-    const { name } = this.state
-
-    this.setState({ name: name === 'Пупа' ? 'Лупа' : 'Пупа' })
-  }
-
-  toggleDummy = () => {
-    this.setState({ dummy: !this.state.dummy })
-  }
-
   render() {
-    const { name } = this.state
-
     return (
-      <React.Fragment>
-        <h1>{name}</h1>
-        <Button className="button_red" onClick={this.toggleMessage}>
-          Изменить сообщение
-        </Button>
-        <button onClick={this.toggleMessage}>Изменить сообщение</button>
-        <button onClick={this.toggleDummy}>Сделать пустое изменение</button>
-      </React.Fragment>
+      <ErrorBoundary>
+        <Bug />
+      </ErrorBoundary>
     )
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import ErrorBoundary from './components/ErrorBoundary'
+
 import App from './components/App'
 import './styles/index.css'
 
 const rootElement = document.getElementById('root') as HTMLDivElement
 const root = createRoot(rootElement)
-root.render(<App />)
+root.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
