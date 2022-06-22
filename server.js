b44f66495b290ec0206601af42932386e90a9fdf
Alexander Senin <isavlex@yandex.ru>
2022-06-22 14:35:12 +0300

    Добавил дефолтный порт

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
+const port = process.env.PORT || 3000
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -10,6 +11,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}!`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
