9a97378b282d2ff44959475c06574b8d7b1bad51
Alexander Senin <isavlex@yandex.ru>
2022-07-01 10:03:31 +0300

    новый конфиг webpack

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
