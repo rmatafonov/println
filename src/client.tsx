f86deca9e48eeb908b2975c7d6d3b3111fb068dd
Alexander Senin <isavlex@yandex.ru>
2022-07-07 14:00:14 +0300

    Исправил конфликты

--- src/client.tsx
+++ src/client.tsx
@@ -27,16 +27,3 @@ hydrateRoot(
   rootElement,
   app
 )
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker
-//       .register('/service-worker.js')
-//       .then((registration) => {
-//         console.log('SW registered: ', registration)
-//       })
-//       .catch((registrationError) => {
-//         console.log('SW registration failed: ', registrationError)
-//       })
-//   })
-// }
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
-=======
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
->>>>>>> 67cf73c5f2fef8b8c250f22645e178aaa53e664e
 import Login from 'pages/Login'
 import SignUp from 'pages/SignUp'
 import Menu from 'pages/Menu'
@@ -12,10 +8,6 @@ import Profile from 'pages/Profile'
 import Forum from 'pages/Forum'
 import ForumTheme from '@/pages/Forum/ForumTheme'
 import ForumAdd from '@/pages/Forum/ForumAdd'
-<<<<<<< HEAD
-=======
-
->>>>>>> 67cf73c5f2fef8b8c250f22645e178aaa53e664e
 import { GameContainer } from '../GameContainer'
 
 import './App.css'
@@ -33,34 +25,17 @@ export default function App() {
   return (
     <div className="app">
       <div className="background"></div>
-<<<<<<< HEAD
-
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/forum" element={<Forum />} />
-        <Route path="/forum/:id" element={<ForumTheme />} />
-        <Route path="/forum/add" element={<ForumAdd />} />
-        <Route path="/game" element={<GameContainer />} />
+        <Route path="/menu" element={<AuthorizedMenu />} />
+        <Route path="/leaderboard" element={<AuthorizedLeaderboard />} />
+        <Route path="/profile" element={<AuthorizedProfile />} />
+        <Route path="/forum" element={<AuthorizedForum />} />
+        <Route path="/forum/:id" element={<AuthorizedForumTheme />} />
+        <Route path="/forum/add" element={<AuthorizedForumAdd />} />
+        <Route path="/game" element={<AuthorizedGameContainer />} />
       </Routes>
-=======
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/menu" element={<AuthorizedMenu />} />
-          <Route path="/leaderboard" element={<AuthorizedLeaderboard />} />
-          <Route path="/profile" element={<AuthorizedProfile />} />
-          <Route path="/forum" element={<AuthorizedForum />} />
-          <Route path="/forum/:id" element={<AuthorizedForumTheme />} />
-          <Route path="/forum/add" element={<AuthorizedForumAdd />} />
-          <Route path="/game" element={<AuthorizedGameContainer />} />
-        </Routes>
-      </Router>
->>>>>>> 67cf73c5f2fef8b8c250f22645e178aaa53e664e
     </div>
   )
 }
